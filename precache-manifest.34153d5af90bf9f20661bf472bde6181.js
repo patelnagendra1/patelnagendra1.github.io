58cc2772bae8d44892d907a589091973f0ecbc4c
Nagendra patel <patelnagendra2915@gmail.com>
2020-09-26 23:43:45 +0530

    Updates

---
+++ precache-manifest.34153d5af90bf9f20661bf472bde6181.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ef2faf7469157e51615a0759cf05c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "285a3a2de155a186f302",
+    "url": "/static/js/2.78fd551d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.78fd551d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57644b37ee074fac096d",
+    "url": "/static/js/main.a0249d5e.chunk.js"
+  },
+  {
+    "revision": "0934e5a0629121a12638",
+    "url": "/static/js/runtime-main.c542aba9.js"
+  },
+  {
+    "revision": "675588da2234673c90476065c3284a6a",
+    "url": "/static/media/Nagendra.675588da.png"
+  },
+  {
+    "revision": "40270502778c0aa7917ba1acf597c76a",
+    "url": "/static/media/Nagendraresume.40270502.pdf"
+  },
+  {
+    "revision": "ac47338d0df8e60744fa9ab8f6228183",
+    "url": "/static/media/Project1.ac47338d.png"
+  },
+  {
+    "revision": "691dbc294df5f16a714a952adda83c52",
+    "url": "/static/media/Project2.691dbc29.png"
+  },
+  {
+    "revision": "c3f7d913fca3c77ea3f63530e158d418",
+    "url": "/static/media/blogApp.c3f7d913.png"
+  },
+  {
+    "revision": "6d6f4b4e325ebd0d6138b355b5b0b916",
+    "url": "/static/media/colorGame.6d6f4b4e.png"
+  },
+  {
+    "revision": "cc01f70f4ff90861fd136182a99c0f24",
+    "url": "/static/media/email_icon.cc01f70f.png"
+  }
+]);
